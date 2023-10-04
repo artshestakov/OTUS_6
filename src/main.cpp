ab0e556f25ecc8e96a20dcbec8b17043cb4935ad
Shestakov Artem <art.shestakov@icloud.com>
2023-10-04 14:11:38 +0300

    Доработка в соответствии с замечаниями преподавателя

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,63 @@
 #include <iostream>
+#include <cassert>
 #include "matrix.h"
 //-----------------------------------------------------------------------------
 int main()
 {
-    Matrix<int, -1> m;
-    m[2][3] = 5;
-    m[2][3] = 7835;
+    //Проверочная часть из условия
+    {
+        // бесконечная матрица int заполнена значениями -1
+        Matrix<int, -1> matrix;
+        assert(matrix.size() == 0); // все ячейки свободны
+        auto a = matrix[0][0];
+        assert(a == -1);
+        assert(matrix.size() == 0);
+        matrix[100][100] = 314;
+        assert(matrix[100][100] == 314);
+        assert(matrix.size() == 1);
 
-    
+        // выведется одна строка
+        // 100100314
+        for (const auto& it : matrix)
+        {
+            size_t row;
+            size_t column;
+            int value;
+            std::tie(row, column, value) = it;
+            std::cout << row << column << value << std::endl;
+        }
+    }
 
-    return 0;
+    //Основное задание
+    {
+        Matrix<int, 0> m;
+
+        //Заполняем диагонали
+        for (int i = 0; i <= 9; ++i)
+        {
+            m[i][i] = i;
+        }
+
+        for (int i = 9; i >= 0; --i)
+        {
+            m[i][9 - i] = i;
+        }
+
+        //Распечатаем матрицу
+        for (int i = 1; i <= 9; ++i)
+        {
+            for (int j = 1; j <= 9; ++j)
+            {
+                std::cout << m[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+
+        //Занятые ячейки
+        std::cout << "Non-default cells: " << m.size() << std::endl;
+    }
+
+    return EXIT_SUCCESS;
 }
 //-----------------------------------------------------------------------------
--- src/matrix.h
+++ src/matrix.h
@@ -1,98 +1,180 @@
 #pragma once
-#ifndef _MATRIX_H_INCLUDED
-#define _MATRIX_H_INCLUDED
 //-----------------------------------------------------------------------------
-#include <vector>
+#include <map>
 //-----------------------------------------------------------------------------
 template <typename T, typename T default_value = T()>
 class Matrix
 {
 private:
-    struct Row
+    class Storage
     {
     private:
-        struct Column
+        struct Key
         {
-            Column(unsigned int column_number)
-                : ColumnNumber(column_number),
-                Value(default_value)
-            {
+            Key(size_t row, size_t column) : Row(row), Column(column) { }
 
+            bool operator<(const Key& k) const
+            {
+                return std::tie(Row, Column) < std::tie(k.Row, k.Column);
             }
 
-            void operator=(T value)
-            {
-                Value = value;
-            }
-            
-            unsigned int ColumnNumber;
-            T Value;
+            size_t Row;
+            size_t Column;
         };
 
     public:
-        Row(unsigned int row_number)
-            : RowNumber(row_number)
-        {
+        using MatrixIterator = typename std::map<Key, T>::iterator;
+        
+        Storage() : m_RowTemp(0), m_ColumnTemp(0) { };
+        ~Storage() = default;
 
+        Storage& operator()(size_t row, size_t column)
+        {
+            m_RowTemp = row;
+            m_ColumnTemp = column;
+            return *this;
         }
 
-        Column& operator[](unsigned int column)
+        void operator=(T value)
         {
-            for (Column& c : m_Columns)
-            {
-                if (c.ColumnNumber == column)
-                {
-                    return c;
-                }
-            }
-            return m_Columns.emplace_back(Column(column));
+            m_Map[Key(m_RowTemp, m_ColumnTemp)] = value;
         }
 
-        unsigned int RowNumber;
+        bool operator==(T value)
+        {
+            return get() == value;
+        }
+
+        operator T()
+        {
+            return get();
+        }
+
+        size_t size() const
+        {
+            return m_Map.size();
+        }
+
+        MatrixIterator begin()
+        {
+            return m_Map.begin();
+        }
+
+        MatrixIterator end()
+        {
+            return m_Map.end();
+        }
 
     private:
-        std::vector<Column> m_Columns;
+        T get()
+        {
+            auto it = m_Map.find(Key(m_RowTemp, m_ColumnTemp));
+            return it == m_Map.end() ?
+                default_value : it->second;
+        }
+
+    private:
+        size_t m_RowTemp;
+        size_t m_ColumnTemp;
+        std::map<Key, int> m_Map;
+    };
+
+    class Wrapper
+    {
+    public:
+        Wrapper() : m_RowTemp(0) { };
+        ~Wrapper() = default;
+
+        Wrapper& operator()(size_t row)
+        {
+            m_RowTemp = row;
+            return *this;
+        }
+
+        Storage& operator[](size_t column)
+        {
+            return m_Storage(m_RowTemp, column);
+        }
+
+        size_t size() const
+        {
+            return m_Storage.size();
+        }
+
+        auto begin()
+        {
+            return m_Storage.begin();
+        }
+
+        auto end()
+        {
+            return m_Storage.end();
+        }
+
+    private:
+        size_t m_RowTemp;
+        Storage m_Storage;
+    };
+
+    class Iterator
+    {
+    public:
+        Iterator(typename Storage::MatrixIterator iter)
+            : m_Iterator(iter)
+        {
+
+        }
+
+        std::tuple<size_t, size_t, T> operator*()
+        {
+            return std::make_tuple(m_Iterator->first.Row, m_Iterator->first.Column, m_Iterator->second);
+        }
+
+        Iterator& operator++()
+        {
+            ++m_Iterator;
+            return *this;
+        }
+
+        bool operator==(const Iterator& other)
+        {
+            return (m_Iterator == other.m_Iterator);
+        }
+
+        bool operator!=(const Iterator& other)
+        {
+            return !(*this == other);
+        }
+
+    private:
+        typename Storage::MatrixIterator m_Iterator;
     };
 
 public:
-    Matrix()
-    {
+    Matrix() = default;
+    ~Matrix() = default;
 
+    Wrapper& operator[](size_t row)
+    {
+        return m_Wrapper(row);
     }
 
-    ~Matrix()
-    {
-
-    }
-
-    //! Получить размер матрицы
-    //! \return возвращает размер матрицы
     size_t size() const
     {
-        return m_Rows.size();
+        return m_Wrapper.size();
     }
 
-    //! Проверить матрицу на пустоту
-    //! \return возвращает true если матрица пустая, иначе - false
-    bool empty() const
+    Iterator begin()
     {
-        return size() == 0;
+        return Iterator(m_Wrapper.begin());
     }
 
-    Row& operator[](unsigned int row)
+    Iterator end()
     {
-        for (Row& r : m_Rows)
-        {
-            if (r.RowNumber == row)
-            {
-                return r;
-            }
-        }
-        return m_Rows.emplace_back(Row(row));
+        return Iterator(m_Wrapper.end());
     }
 
 private:
-    std::vector<Row> m_Rows;
+    Wrapper m_Wrapper;
 };
 //-----------------------------------------------------------------------------
-#endif
